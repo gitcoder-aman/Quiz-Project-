098226c3a48ed85f876b54fc823f617ee884fa40
Aman <amankumar93578@gmail.com>
2022-08-14 10:50:05 +0530

    Added Time down counter feature in Quiz App

--- app/src/main/java/com/example/quizearn/QuizActivity.java
+++ app/src/main/java/com/example/quizearn/QuizActivity.java
@@ -2,6 +2,8 @@ package com.example.quizearn;
 
 import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 
+import static java.security.AccessController.getContext;
+
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
@@ -14,7 +16,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.quizearn.databinding.ActivityQuizBinding;
 
 import java.util.ArrayList;
-import java.util.Optional;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -25,7 +26,7 @@ public class QuizActivity extends AppCompatActivity {
     int countClick = 0;
     int checkClick = 0;
     Question question;
-    CountDownTimer timer = null;
+    CountDownTimer countDownTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,27 +38,41 @@ public class QuizActivity extends AppCompatActivity {
         questions.add(new Question("What is Earth?","Planet","Sun","Human","Car","Planet"));
         questions.add(new Question("What is Samosa?","Planet","Food","Human","Car","Food"));
 
-        resetTimer();
+        StartTimer();
         setNextQuestion();
-
     }
 
-    void resetTimer(){
-         timer = new CountDownTimer(30000,1000) {
+    private void StartTimer(){
+         countDownTimer = new CountDownTimer(30000,1000) {
             @Override
             public void onTick(long millisUntilFinished) {
-                binding.timer.setText((int) (millisUntilFinished/1000));
+                binding.timer.setText(String.valueOf(millisUntilFinished/1000));
             }
 
              @Override
              public void onFinish() {
-//                 Toast.makeText(QuizActivity.this, "Your time has End", Toast.LENGTH_SHORT).show();
-                 Log.i(TAG,"Time finished");
+                 if(index < questions.size()) {
+                     index++;
+
+                     if(index != questions.size()){ //when we will reached last position of question then restOption will not call.
+
+                         resetOption();
+                         Toast.makeText(QuizActivity.this, "Quiz Finished.", Toast.LENGTH_SHORT).show();
+                     }
+
+                     if(checkClick == 0) //if user no check any option then handle
+                         countClick++;
+
+                     setNextQuestion();
+                 }
+
+                 Toast.makeText(QuizActivity.this,"Your time has End", Toast.LENGTH_SHORT).show();
             }
         };
+
     }
 
-    void showAnswer(){
+    private void showAnswer(){
         if(question.getAnswer().equals(binding.option1.getText().toString()))
             binding.option1.setBackground(getResources().getDrawable(R.drawable.option_right));
         else if(question.getAnswer().equals(binding.option2.getText().toString()))
@@ -67,13 +82,13 @@ public class QuizActivity extends AppCompatActivity {
         else if(question.getAnswer().equals(binding.option4.getText().toString()))
             binding.option4.setBackground(getResources().getDrawable(R.drawable.option_right));
     }
-    void setNextQuestion(){
+    private void setNextQuestion(){
 
-        if(timer != null){
-            timer.cancel();
+        if(countDownTimer!= null){
+            countDownTimer.cancel();
         }
-//        Log.d("aman","Hey Error Found");
-        timer.start(); //here have a error
+
+        countDownTimer.start(); //here have a error
 
         if(index < questions.size()){
             binding.questionCounter.setText(String.format("%d/%d",(index+1),questions.size())); //show how many question are remaining.
@@ -84,11 +99,15 @@ public class QuizActivity extends AppCompatActivity {
             binding.option2.setText(question.getOption2());
             binding.option3.setText(question.getOption3());
             binding.option4.setText(question.getOption4());
+
+        }
+        else{
+            countDownTimer.cancel();
         }
     }
 
 
-    void checkAnswer(TextView textView){
+    private void checkAnswer(TextView textView){
         String selectedAnswer = textView.getText().toString();
         if(selectedAnswer.equals(question.getAnswer())){
             textView.setBackground(getResources().getDrawable(R.drawable.option_right));
@@ -99,12 +118,13 @@ public class QuizActivity extends AppCompatActivity {
             Toast.makeText(this, "Wrong Answer", Toast.LENGTH_SHORT).show();
         }
     }
-    void resetOption(){
+    private void resetOption(){
         binding.option1.setBackground(getResources().getDrawable(R.drawable.option_unselected));
         binding.option2.setBackground(getResources().getDrawable(R.drawable.option_unselected));
         binding.option3.setBackground(getResources().getDrawable(R.drawable.option_unselected));
         binding.option4.setBackground(getResources().getDrawable(R.drawable.option_unselected));
     }
+    //onClink on Option and nextBtn
     public void onClick(View view){
 
         switch(view.getId()){
@@ -115,7 +135,7 @@ public class QuizActivity extends AppCompatActivity {
 
                 if(countClick == index){  //if one option is selected then other option is not allowed to check.
 
-                    if(timer != null) timer.cancel(); //timer stop
+                    if(countDownTimer != null) countDownTimer.cancel(); //timer stop
 
                     TextView selected1 = (TextView) view;
                     checkAnswer(selected1);
@@ -128,10 +148,10 @@ public class QuizActivity extends AppCompatActivity {
                 if(index < questions.size()) {
                     index++;
 
-                    if(index != questions.size())
+                    if(index != questions.size()) // when we reach last Position of question then not resetOption will be call.
                       resetOption();
 
-                    if(checkClick == 0)
+                    if(checkClick == 0) //if user no check any option then handle.
                         countClick++;
 
                     setNextQuestion();
